f810ad01b46f306c3b1e34ba9ba3fcadfac37755
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-01 21:49:23 -0700

    app crashed

---
+++ controllers/hotelController.js
@@ -0,0 +1,3 @@
+exports.homePage = (req, res) => {
+  res.render('index', { title: "Let's Travel" });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "Let's Travel" });
-});
+router.get('/', hotelController.homePage);
 
 router.get('/all', function(req, res) {
   res.render('all_hotels', { title: "All Hotels" });
