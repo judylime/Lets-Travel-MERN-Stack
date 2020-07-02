47c9f188da15d6a656996cfe11cefc98a1a69e60
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-01 19:00:22 -0700

    npm run devstart

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: "Let's Travel" });
-  res.send('hello');
+  res.render('index', { title: "Let's Travel" });
 });
 
 module.exports = router;
