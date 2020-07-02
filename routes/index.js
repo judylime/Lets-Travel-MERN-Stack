042d9de4f9ea475997d2d2daaf3b5f2c871af66a
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-01 21:50:44 -0700

    require controllers

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-//
+//require controllers:
+const hotelController = require ('../controllers/hotelController');
 /* GET home page. */
 router.get('/', hotelController.homePage);
 
