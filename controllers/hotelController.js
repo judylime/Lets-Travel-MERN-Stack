4c058c096f945aaa899eb21cbe0ef491aa7c0e84
JudyLime <56984748+judylime@users.noreply.github.com>
2020-07-02 22:34:35 -0700

    npm i multer

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,4 +1,11 @@
 const Hotel = require('../models/hotel');
+const cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
 // const { NotExtended } = require('http-errors');
 
 // exports.homePage = (req, res) => {
